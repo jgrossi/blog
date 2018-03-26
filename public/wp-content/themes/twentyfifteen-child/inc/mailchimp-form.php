963d4f0d27998c2e62dc3269d5d04863c48bb074
Junior Grossi <juniorgro@gmail.com>
2018-03-26 16:12:08 -0300

    Add Mailchimp include file

---
+++ public/wp-content/themes/twentyfifteen-child/inc/mailchimp-form.php
@@ -0,0 +1,14 @@
+<?php 
+
+if ( is_single() ) {
+	$query = new WP_Query( array ( 
+		'post_type' => 'mc4wp-form',
+		'posts_per_page' => 1,
+	) );
+
+	$posts = $query->get_posts();
+
+	if ( $posts && $post = $posts[0] ) {
+		echo do_shortcode( '[mc4wp_form id="'.$post->ID.'"]' );
+	}
+}
